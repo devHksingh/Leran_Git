522c0427e7058646afda4b1f4799e4d2c488433a
Ankitpha <pharswanankit27@gmail.com>
2025-05-24 12:51:26 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, consectetur?</p>
       </div>
     </div>
   );
